4b249098ca2a3268b63fe8338db82d5aa69c9fff
viktoriakazanskaya <vikamur_555@mail.ru>
2024-02-22 02:55:57 +0400

    задача 3

--- seminar01/task03/Program.cs
+++ seminar01/task03/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+
+Console.Write("Введите целое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num%10;
+int num2 = num/100;
+Console.WriteLine($"Сумма {num2} и {num1} равна {num1+num2}");
+
+
+
+
